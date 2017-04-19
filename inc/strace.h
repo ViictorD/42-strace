04f8f722871ed3b4e904974e01a4d6b5f8741ed8
Rodolphe CARGOU <rcargou@e1r13p7.42.fr>
2017-04-19 20:24:12 +0200

    ouais

--- inc/strace.h
+++ inc/strace.h
@@ -6,7 +6,7 @@
 /*   By: rcargou <rcargou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/19 17:51:43 by rcargou           #+#    #+#             */
-/*   Updated: 2017/04/19 20:09:36 by rcargou          ###   ########.fr       */
+/*   Updated: 2017/04/19 20:23:59 by rcargou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 # include <string.h>
 # include <stdlib.h>
 # include <stdio.h>
+#include <signal.h>
 
 int		get_complete_path(char ret[1024], const char *filename);
 void	exec_trace(char *path, char **a);
