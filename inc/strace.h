0b3a005e54f5f5bdbb72fb330e6490d16bde634d
Rodolphe CARGOU <rcargou@e1r12p13.42.fr>
2017-04-20 20:01:56 +0200

    dqw

--- inc/strace.h
+++ inc/strace.h
@@ -6,7 +6,7 @@
 /*   By: rcargou <rcargou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/19 17:51:43 by rcargou           #+#    #+#             */
-/*   Updated: 2017/04/20 19:16:56 by rcargou          ###   ########.fr       */
+/*   Updated: 2017/04/20 20:01:06 by rcargou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,28 @@
 # include <string.h>
 # include <stdlib.h>
 # include <stdio.h>
-#include <signal.h>
+# include <signal.h>
+
+enum e_type
+{
+	int_,
+	uint_,
+	long_,
+	ulong_,
+	pointer_,
+	string_,
+	array_,
+};
+
+typedef enum e_type type;
+
+typedef struct s_info;
+{
+	const char	*name;
+	type		ret;
+	unsigned	arg_num;
+	type args_type[24];
+}	t_info;
 
 int		get_complete_path(char ret[1024], const char *filename);
 void	exec_trace(char *path, char **av, char **env);
