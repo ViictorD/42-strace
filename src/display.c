e5e908f2aef76786dd45983490cb6fac26b08e69
Rodolphe CARGOU <rcargou@e1r12p13.42.fr>
2017-04-21 16:48:52 +0200

    oui

--- src/display.c
+++ src/display.c
@@ -6,7 +6,7 @@
 /*   By: rcargou <rcargou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/21 16:26:53 by rcargou           #+#    #+#             */
-/*   Updated: 2017/04/21 16:46:57 by rcargou          ###   ########.fr       */
+/*   Updated: 2017/04/21 16:48:50 by rcargou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,5 @@ void display_syscall(long id, void **args)
 	fprintf(stderr, "%s ", info.name);
 	for (i = 0; i < info.arg_num; i++)
 		print_arg(info.args_type[i], args[i]);
+	fprintf(stderr, "\n");
 }
