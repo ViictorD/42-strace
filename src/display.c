1920cc0b6f633f6a5c22a441f35a6ce04d7bd2c9
Rodolphe CARGOU <rcargou@e1r12p13.42.fr>
2017-04-21 16:42:34 +0200

    oui

--- src/display.c
+++ src/display.c
@@ -6,7 +6,7 @@
 /*   By: rcargou <rcargou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/21 16:26:53 by rcargou           #+#    #+#             */
-/*   Updated: 2017/04/21 16:41:15 by rcargou          ###   ########.fr       */
+/*   Updated: 2017/04/21 16:42:32 by rcargou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void print_arg(type type, void *value)
 {
-	printf("%d\n", (int)value);
+	fprintf(stderr, "%d\n", (int)value);
 }
 
 void display_syscall(long id, void **args)
@@ -22,7 +22,7 @@ void display_syscall(long id, void **args)
 	t_info info;
 
 	info = get_info(id);
-	fprintf(2, "%s ", info.name);
+	fprintf(stderr, "%s ", info.name);
 	//for (int i = 0; i < info.arg_num; i++)
 	//	print_arg(info.args_type[i], args[i]);
 }
