9eba0e39a7d8e231146e64e6d6841e31cc213cfa
Rodolphe CARGOU <rcargou@e1r12p13.42.fr>
2017-04-24 13:26:13 +0200

    yolo

--- src/display.c
+++ src/display.c
@@ -6,7 +6,7 @@
 /*   By: rcargou <rcargou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/21 16:26:53 by rcargou           #+#    #+#             */
-/*   Updated: 2017/04/24 13:25:46 by rcargou          ###   ########.fr       */
+/*   Updated: 2017/04/24 13:26:12 by rcargou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,7 @@ void display_syscall(long id, void **args)
 	t_info info;
 	int i;
 	info = get_info(id);
-	fprintf(stderr, "( %s ", info.name);
+	fprintf(stderr, "%s( ", info.name);
 	for (i = 0; i < info.arg_num; i++)
 		print_arg(info.args_type[i], args[i]);
 	fprintf(stderr, ") ");
