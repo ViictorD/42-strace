a7242b665e50ff9a20a0fe992bd2e44db3a58989
Rodolphe CARGOU <rcargou@e1r12p13.42.fr>
2017-04-24 13:23:21 +0200

    yolo

--- src/get_data.c
+++ src/get_data.c
@@ -6,7 +6,7 @@
 /*   By: rcargou <rcargou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/20 19:06:21 by rcargou           #+#    #+#             */
-/*   Updated: 2017/04/24 13:22:48 by rcargou          ###   ########.fr       */
+/*   Updated: 2017/04/24 13:23:18 by rcargou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ long get_word(pid_t pid, long w)
 {
 	long data;
 
-	data = ptrace(PTRACE_PEEKDATA, pid, w * sizeof(long), NULL);
+	data = ptrace(PTRACE_PEEKDATA, pid, w, NULL);
 	return (data);
 }
 
@@ -43,7 +43,7 @@ char *get_string(pid_t pid, long addr)
 	string[0] = 0;
 	for (i = 0; i < 256; i++)
 	{
-		data = get_word(pid, addr + i);
+		data = get_word(pid, addr + i * 4);
 		memcpy(&string[i * 4], &data, sizeof(long));
 		for (j = 0; j < sizeof(long) / 8; j++)
 			if (&((char*)data)[j] == '\0')
