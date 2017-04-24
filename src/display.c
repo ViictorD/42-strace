6023f476270dd6c7f2d8267d80194fcbfec0bc8b
Rodolphe CARGOU <rcargou@e1r12p13.42.fr>
2017-04-24 13:31:31 +0200

    yolo

--- src/display.c
+++ src/display.c
@@ -6,7 +6,7 @@
 /*   By: rcargou <rcargou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/21 16:26:53 by rcargou           #+#    #+#             */
-/*   Updated: 2017/04/24 13:30:52 by rcargou          ###   ########.fr       */
+/*   Updated: 2017/04/24 13:31:29 by rcargou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ void print_string(char *str)
 
 	for (i = 0; str[i]; i++)
 	{
-		if (str[i] > ' ' && str[i] < 127)
+		if (str[i] > 0 && str[i] < 127)
 			fprintf(stderr, "%c", str[i]);
 		else
 			fprintf(stderr, "\\%d", str[i]);
