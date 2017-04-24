624691946c127bbe1556e50d144b749d50727ea3
Rodolphe CARGOU <rcargou@e1r12p13.42.fr>
2017-04-24 13:17:45 +0200

    yolo

--- src/get_data.c
+++ src/get_data.c
@@ -6,7 +6,7 @@
 /*   By: rcargou <rcargou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/20 19:06:21 by rcargou           #+#    #+#             */
-/*   Updated: 2017/04/21 19:10:08 by rcargou          ###   ########.fr       */
+/*   Updated: 2017/04/24 13:17:29 by rcargou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,44 @@ long get_word_user(pid_t pid, long w)
 	return (data);
 }
 
+long get_word(pid_t pid, long w)
+{
+	long data;
+
+	data = ptrace(PTRACE_PEEKDATA, pid, w, NULL);
+	return (data);
+}
+
 void *get_data_value(pid_t pid, type type, long value)
 {
 	return ((void*)value);
+}
+
+char *get_string(pid_t pid, long addr)
+{
+	long data;
+	char *string;
+	unsigned i;
+	int next;
+
+	string = (char *)malloc(sizeof(char) * (256 * 4 + 1));
+	string[0] = 0;
+	for (i = 0; i < 256; i++)
+	{
+		data = get_word(pid, addr + i);
+		memcpy(&string[i * 4], data, sizeof(long));
+		for (int j = 0; j < sizeof(long) / 8; j++)
+			if (&((char*)data)[j] == '\0')
+				break ;
+	}
+	return (string);
 }
 
 void get_args(pid_t pid, long id, void  **out)
 {
 	t_info		info;
 	long		data;
-	int i;
+	int			i;
 
 	info = get_info(id);
 	for (i = 0; i < info.arg_num; i++)
